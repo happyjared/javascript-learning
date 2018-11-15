602983be02efc4b51fdfeb535317219e5575a1d4
happyJared <happyjared1995@gmail.com>
2018-11-15 18:19:15 +0800

    add iview date-picker

--- wechat-mp/js/mp.js
+++ wechat-mp/js/mp.js
@@ -16,6 +16,37 @@ new Vue({
         articleList: [],
         input_value: '',
         screenWidth: document.body.clientWidth, // 屏宽
+        dateOptions: {
+            shortcuts: [
+                {
+                    text: '近1周内',
+                    value() {
+                        const end = new Date();
+                        const start = new Date();
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+                        return [start, end];
+                    }
+                },
+                {
+                    text: '近1个月',
+                    value() {
+                        const end = new Date();
+                        const start = new Date();
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+                        return [start, end];
+                    }
+                },
+                {
+                    text: '近3个月',
+                    value() {
+                        const end = new Date();
+                        const start = new Date();
+                        start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+                        return [start, end];
+                    }
+                }
+            ]
+        }
     },
     created() {
         let _this = this;
