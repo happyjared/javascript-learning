195dc3e594f3676f0f8839938a4c52073438c988
Jared <jiandong.qiu@qlchat.com>
2019-08-13 14:31:07 +0800

    update

--- jianshu-bsc/js/bsc.js
+++ jianshu-bsc/js/bsc.js
@@ -12,7 +12,7 @@ new Vue({
         screenWidth: document.body.clientWidth,
     },
     created() {
-     this.loadArticle();
+     this.init();
     },
     mounted: function () {
         this.screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
@@ -23,26 +23,32 @@ new Vue({
     computed: {},
     watch: {},
     methods: {
+        init: function () {
+            if(!this.isWeiXin()){
+                layer.ready(res => {
+                    var index = layer.load(1, {
+                        time: 3000,
+                        shade: [0.3, '#0b0b0b'],
+                    });
+                    this.getApi(index);
+                });
+            }else{
+                this.getApi(-1);
+            }
+        },
         // 请求API
-        loadArticle: function () {
-//            let lodeAnimate = layer.load(1, {
-//                shade: [0.3, '#0b0b0b']
-//            });
-            axios.post(this.getApi()).then(response => {
+        getApi: function (index) {
+            let api = this.domainName + '/rank/';
+            axios.post(api).then(response => {
                 this.rankList = response.data;
                 this.size = this.rankList.length;
                 console.log('RankList size: ' + this.size);
             }).catch(function (error) {
                 console.log('Request API Error ' + error);
             }).finally(function () {
-//                layer.close(lodeAnimate);
+                layer.close(index);
+                layer.closeAll();
             });
-//            layer.closeAll();
-        },
-        getApi: function () {
-            let api = this.domainName + '/rank/';
-            console.log('Request API URL ' + api);
-            return api;
         },
         getTableColor: function (index) {
             if (index === 0) {
@@ -57,12 +63,8 @@ new Vue({
                 return "table-secondary";
             }
         },
-        isWeiXin:function (){
-            var ua = window.navigator.userAgent.toLowerCase();
-            if(ua.match(/MicroMessenger/i) == 'micromessenger'){
-                return true;
-            }
-            return false;
+        isWeiXin: function (){
+            return navigator.userAgent.toLowerCase().indexOf('micromessenger') > -1;
         },
     },
 });
